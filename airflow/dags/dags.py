67a1e71cd7e3ea336dfb9553e898a35105760503
chriscardillo <cfcardillo23@gmail.com>
2023-03-12 11:32:12 -0400

    use create_dags

--- airflow/dags/dags.py
+++ airflow/dags/dags.py
@@ -1,25 +1,21 @@
 import os
 import airflow
-from gusty import create_dag
+from gusty import create_dags
 
 #####################
 ## DAG Directories ##
 #####################
 
 # point to your dags directory
-dag_parent_dir = os.path.join(os.environ['AIRFLOW_HOME'], "dags")
+dag_parent_dir = os.path.join(os.environ["AIRFLOW_HOME"], "dags")
 
-# assumes any subdirectories in the dags directory are Gusty DAGs (with METADATA.yml) (excludes subdirectories like __pycache__)
-dag_directories = [os.path.join(dag_parent_dir, name) for name in os.listdir(dag_parent_dir) if os.path.isdir(os.path.join(dag_parent_dir, name)) and not name.endswith('__')]
-
-####################
-## DAG Generation ##
-####################
-
-for dag_directory in dag_directories:
-    dag_id = os.path.basename(dag_directory)
-    globals()[dag_id] = create_dag(dag_directory,
-                                   tags = ['default', 'tags'],
-                                   task_group_defaults={"tooltip": "this is a default tooltip"},
-                                   wait_for_defaults={"retries": 10, "check_existence": True},
-                                   latest_only=False)
+# create_dags (plural) is great for when you have multiple dags to create
+# create_dag (singular) is also available for when you want to just create one dag
+create_dags(
+    dag_parent_dir,
+    globals(),
+    tags=["default", "tags"],
+    task_group_defaults={"tooltip": "this is a default tooltip"},
+    wait_for_defaults={"retries": 10, "check_existence": True},
+    latest_only=False,
+)
